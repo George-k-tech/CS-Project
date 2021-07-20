b2be0814a1d75d7dddf4d92a04e1705d2eb50660
George <kariukigeorge511@gmail.com>
2021-07-20 23:32:07 +0300

    Authentication module

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//auth route for both user and the admin
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name ('dashboard');
+});
+
+//auth route for the users
+Route::group(['middleware' => ['auth', 'role:user']], function(){
+    Route::get('/dashboard/myprofile', 'App\Http\Controllers\DashboardController@myprofile')->name ('dashboard.myprofile');
+});
+
+// auth route for the admin only
+Route::group(['middleware' => ['auth', 'role:admin']], function(){
+    Route::get('/admin/index', 'App\Http\Controllers\AdminController@index')->name ('admin.index');
+});
+
+require __DIR__.'/auth.php';
